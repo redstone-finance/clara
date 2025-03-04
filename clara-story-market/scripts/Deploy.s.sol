19c039310f4cb119d70b38c32693faa97fd1becd
Tadeuchi <tadeuszwl@gmail.com>
2025-03-04 09:26:18 +0100

    market: deploy proxy

--- clara-story-market/scripts/Deploy.s.sol
+++ clara-story-market/scripts/Deploy.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
-import {ClaraMarketV1} from "../src/ClaraMarket.sol";
+import {ClaraMarketV1} from "../src/ClaraMarketV1.sol";
 
 contract DeployScript is Script {
     function setUp() public {}
@@ -11,12 +11,21 @@ contract DeployScript is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        address sUSD = 0xC0F6E387aC0B324Ec18EAcf22EE7271207dCE3d5;
+        
+        address ipAssetRegistry = 0x77319B4031e6eF1250907aa00018B8B1c67a244b;
+        address licensingModule = 0x04fbd8a2e56dd85CFD5500A4A4DfA955B9f1dE6f;
+        address pilTemplate = 0x2E896b0b2Fdb7457499B56AAaA4AE55BCB4Cd316;
+        address royaltyPolicyLAP = 0xBe54FB168b3c982b7AaE60dB6CF75Bd8447b390E;
+        address royaltyWorkflows = 0x9515faE61E0c0447C6AC6dEe5628A2097aFE1890;
+        address royaltyModule = 0xD2f60c40fEbccf6311f8B47c4f2Ec6b040400086;
+        address payable _revenueToken = payable(0x1514000000000000000000000000000000000000);
 
         address transparentProxy = Upgrades.deployTransparentProxy(
-            "ClaraMarket.sol",
+            "ClaraMarketV1.sol",
             msg.sender,
-            abi.encodeCall(ClaraMarketV1.initialize, (sUSD))
+            abi.encodeCall(
+                ClaraMarketV1.initialize, 
+                (ipAssetRegistry, licensingModule, pilTemplate, royaltyPolicyLAP, royaltyWorkflows, royaltyModule, _revenueToken))
         );
     }
 }
